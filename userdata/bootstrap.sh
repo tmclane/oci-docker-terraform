f1ac0cd38ff572e4c18b6d244f403d45ba25b281
Travis McLane <travis.mclane@synacor.com>
2018-08-04 14:30:47 -0500

    disable firewall + start docker properly

--- userdata/bootstrap.sh
+++ userdata/bootstrap.sh
@@ -39,12 +39,17 @@ sudo yum install -y docker-ce 2>&1 | tee -a $LOGFILE
 sudo usermod -aG docker opc  | tee -a $LOGFILE
 
 # Update firewall
+sudo firewall-cmd --set-default-zone=trusted | tee -a $LOGFILE
+
+# Add ens3 to trusted zone
+sudo firewall-cmd --zone=trusted --add-interface=ens3 --permanent | tee -a $LOGFILE
+
 sudo firewall-cmd --add-port=2376/tcp --add-port=2377/tcp --add-port=7946/tcp --add-port=7946/udp --add-port=4789/udp --permanent  2>&1 | tee -a $LOGFILE
 
 sudo firewall-cmd --reload 2>&1 | tee -a $LOGFILE
 
 # Restart to let docker add it's iptable rules
-sudo systemctl restart docker 2>&1 | tee -a $LOGFILE
+sudo systemctl start docker 2>&1 | tee -a $LOGFILE
 
 echo true > /opt/zimbra/.firstboot.status
 
